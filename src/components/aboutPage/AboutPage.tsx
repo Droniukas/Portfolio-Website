0eb58af99b3797fa6be49ed8528aea85fdbcb7de
Domas <doremis.lt@gmail.com>
2023-10-22 23:33:44 +0300

    optimized aboutpage image

--- src/components/aboutPage/AboutPage.tsx
+++ src/components/aboutPage/AboutPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import SectionSeperator from '../sectionSeperator/SectionSeperator';
-import profilePhoto from './resources/images/profile-photo-mid.jpg';
+import profilePhoto from './resources/images/profile-photo-mid-2.jpg';
 import './AboutPage.css';
 import { calculateMiddleDivLeftSideWidth } from './Utils';
 import { gsap } from '../../config/gsap';
