53d860e56610242ae2b4e834227632071228403f
Domas <doremis.lt@gmail.com>
2023-11-06 10:33:00 +0200

    remove security risk in sidebar links

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -11,13 +11,17 @@ const Sidebar = () => {
     <div id="sidebar-div">
       <Logo />
       <div id="sidebar-icons">
-        <a href="https://github.com/Droniukas" target="_blank">
+        <a href="https://github.com/Droniukas" target="_blank" rel="noopener noreferrer">
           <div className="sidebar-img-container">
             <img src={githubIconGradient} className="sidebar-img" />
             <img src={githubIconBlack} className="sidebar-img initial-sidebar-img" />
           </div>
         </a>
-        <a href="https://www.linkedin.com/in/domas-raila-54294b180/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/domas-raila-54294b180/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <div className="sidebar-img-container">
             <img src={linkedinIconGradient} className="sidebar-img" />
             <img src={linkedinIconBlack} className="sidebar-img initial-sidebar-img" />
