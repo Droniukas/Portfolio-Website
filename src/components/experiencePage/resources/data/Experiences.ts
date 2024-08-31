330ea7e1d9e870a9aac3311d1e1afe29c00c3238
Domas Raila <doremis.lt@gmail.com>
2024-08-31 12:16:54 +0300

    UPDATE EXPERIENCES

--- src/components/experiencePage/resources/data/Experiences.ts
+++ src/components/experiencePage/resources/data/Experiences.ts
@@ -1,33 +1,37 @@
 import { Experience } from '../../../../interfaces/Experience.interface';
-import selfTaughtDev from '../images/self-tought/self-tought-dev.jpg';
-import selfTaughtDevSmall from '../images/self-tought/self-tought-dev-small.jpg';
 import devbridgeAcademy from '../images/devbridge/devbridge-academy.jpg';
 import devbridgeAcademy2 from '../images/devbridge/devbridge-academy-2.jpg';
 import xplicityAcademy from '../images/xplicity/xplicity-academy.jpg';
 import xplicityAcademySmall from '../images/xplicity/xplicity-academy-small.jpg';
 import xplicityAcademy2 from '../images/xplicity/xplicity-academy-2.jpg';
 import devbridgeAcademySmall from '../images/devbridge/devbridge-academy-small.jpg';
+import groupOfficePhotoIntus from '../images/intus/group-office-phoyo.png'
+import presentationIntus from '../images/intus/presentation.jpg'
+
 
 export const experiences: Experience[] = [
   {
     id: 1,
     images: [
       {
-        image: selfTaughtDev,
-        smallImage: selfTaughtDevSmall,
+        image: presentationIntus,
+        smallImage: presentationIntus,
       },
+      {
+        image: groupOfficePhotoIntus,
+      }
     ],
-    name: 'Self-taught programmer',
-    date: 'from 2022',
-    place: 'Šiauliai, Lithuania',
+    name: 'Software developer at Intus Windows',
+    date: 'from Dec 2023',
+    place: 'Remote/Šiauliai lithuania',
     description:
-      'I started on my self-thought path by taking online courses and building small projects.',
+      "This is where I started my proffesional career. Here I've worked on multiple projects, and met lot's of cool people.",
     modal: {
       longerDescription: `
-      Somewhere around the middle of 2022, I decided to switch from 3D design to programming, 
-      for the first few months I started by learning Python - my first OOP language. 
-      Throughout that time I learned from online content and built some small Windows applications. 
-      Although I liked the language I got interested in how the web works and got into full-stack web dev. From there I started learning the basics of CSS, HTML, Javascript, Java and SQL and built small projects.      `,
+        Intus Windows is one of the main window manufacturers in the USA that has an office/factory in Lithuania. 
+        Here I worked on a large range of different projects, had to work with many different teams and departments of the company, met lots of people, and learned a lot about other systems. 
+        I'm very thankful for my manager who put me on different projects, not only letting me work with different teams but also letting me develop an application where I was the only developer from start to finish. 
+        The general technologies I worked with were mainly React, Angular, and .Net/C#. Overall the company had a great culture, and people were great fun to work with.`,
     },
   },
   {
@@ -40,14 +44,14 @@ export const experiences: Experience[] = [
     date: '2023 March - 2023 May',
     place: 'Kaunas, Lithuania',
     description:
-      'Here I got hands-on experience working in an Agile team and built a full-stack web application with Spring boot and React.',
+      'Here me and the team built a full-stack web application with Spring and React in 3 months.',
     modal: {
       longerDescription: `
-      Here, with the team, we created a web application that won first place. 
-      I gained valuable experience working in a team, participating in meetings, and helping other colleagues. 
+      Here, me and other aspiring developers created a web application that won first place. 
+      Throught the academy we worked together trying to succesfully create a the application in our 3 month deadline. 
       I also got to participate in workshops about DevOps and testing. 
       One of my favorite parts of the project I was responsible for was a real-time notification system that used a WebSocket connection. 
-      The main technologies we used were React, TypeScript, Java with Spring Boot, PostgreSQL, Jira (Agile), and Git.
+      The main technologies we used were React, Java with Spring.
       `,
     },
   },
@@ -66,7 +70,7 @@ export const experiences: Experience[] = [
       Here with the team, we developed a full-stack web application that also won first place! 
       Apart from the main project we also participated in team-building events and multiple lectures. 
       We had to work fast which led to us spending a lot of time working together. 
-      Here we mainly used ASP.NET, Angular and Azure DevOps.
+      Here we mainly used .NET/C#, Angular and Azure DevOps.
       `,
     },
   },
